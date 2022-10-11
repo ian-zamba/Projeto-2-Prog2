6fe188eda535cd918dbdbf13dd444943e80b69bc
Ian Zambanini <ianmtzb@gmail.com>
2022-10-11 15:44:37 -0300

    loop escolha errada arrumada

--- Main.cpp
+++ Main.cpp
@@ -8,7 +8,7 @@ int main(void) {
     char* caminhodat = "receitas.dat";
     char* caminhotxt = "receitas.txt";
     //Declarando a variável de opção
-    int opcao;
+    char opcao;
 
     //Menu
     do{
@@ -16,27 +16,25 @@ int main(void) {
         cout << "2 - Listar receitas" << endl;
         cout << "3 - Buscar receita" << endl;
         cout << "4 - Sair" << endl;
-        cout << "Digite a opcao desejada: ";
-        cin >> opcao;
-        cin.ignore();
+        opcao = getch();
         switch (opcao){
-            case 1:
+            case '1':
                 Cadastro(caminhodat, caminhotxt);
                 break;
-            case 2:
+            case '2':
                 Listar(caminhodat, caminhotxt);
                 break;
-            case 3:
+            case '3':
                 Buscar(caminhodat, caminhotxt);
                 break;
-            case 4:
+            case '4':
                 cout << "Saindo..." << endl;
                 break;
             default:
-                cout << "Opcao invalida!" << endl;
+                cout << "\nOpcao invalida!\n" << endl;
                 break;
         }
-    } while (opcao != 4);
+    } while (opcao != '4');
 
 
     return 0;
